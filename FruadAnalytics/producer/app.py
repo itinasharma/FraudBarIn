544e46be3de81b129cdd6d2ecb458265e6345189
getmubarak <getmub@gmail.com>
2024-04-01 12:58:58 +0530

    Update app.py

--- FruadAnalytics/producer/app.py
+++ FruadAnalytics/producer/app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
    
 @app.route("/add", methods = ['POST'])
 def add_contact() :
-    data = json.loads(request.data)
+    data = request.get_json()
+    #data = json.loads(request.data)
     producer = KafkaProducer(bootstrap_servers='kafka:9092')
     producer.send('my-topic', json.dumps(data).encode('utf-8'))
     return "ok"
     
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
-
