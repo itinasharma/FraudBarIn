6e3d478e186e8fff84cf42f23c7613e3c64b2164
getmubarak <getmub@gmail.com>
2024-04-14 22:01:30 +0530

    Update MLModelTrain.py

--- MLModelTrain.py
+++ MLModelTrain.py
@@ -3,6 +3,7 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
+from pyspark.sql.functions import from_json, col
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql.types import *
@@ -13,11 +14,11 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Sample data, replace this with your actual data
-data = [("01-01-2023 08:00","TXN1127", "ACC4", "95071.92", "MerchantH", "Purchase", "Tokyo"),
-        ("01-01-2023 08:01","TXN1639","ACC10", "15607.89", "MerchantH", "Purchase", "London"),
-        ("01-01-2023 08:02","TXN1127","ACC8", "65092.34", "MerchantE", "Withdrawal", "London"),
-        ("01-01-2023 08:03","TXN872","ACC6", "87.87", "MerchantE", "Purchase", "London"),
-        ("01-01-2023 08:04","TXN1338","ACC4", "716.56", "MerchantI", "Purchase", "Los Angeles")]
+data = [("01-01-2023 08:00","TXN1127", "ACC4", 95071.92, "MerchantH", "Purchase", "Tokyo"),
+        ("01-01-2023 08:01","TXN1639","ACC10", 15607.89, "MerchantH", "Purchase", "London"),
+        ("01-01-2023 08:02","TXN1127","ACC8", 65092.34, "MerchantE", "Withdrawal", "London"),
+        ("01-01-2023 08:03","TXN872","ACC6", 87.87, "MerchantE", "Purchase", "London"),
+        ("01-01-2023 08:04","TXN1338","ACC4", 716.56, "MerchantI", "Purchase", "Los Angeles")]
 
 schema = ["Timestamp","TransactionID", "AccountID", "Amount", "Merchant", "TransactionType","Location"]
 df = spark.createDataFrame(data, schema)
@@ -48,5 +49,3 @@ model.save(model_path)
 
 # Stop the SparkSession
 spark.stop()
-
-        
